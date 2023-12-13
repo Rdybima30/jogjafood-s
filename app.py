f70c6673a6c1e622c5e69e3cf5b9c022947a273e
Arifsand <muhamadsan@yandex.com>
2023-12-13 20:43:51 +0700

    sungkem puh

--- app.py
+++ app.py
@@ -11,49 +11,61 @@ from dotenv import load_dotenv
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/addmenu')
 def add_menu():
     return render_template('addmenu.html')
 
+
 @app.route('/detail')
 def detail():
     return render_template('detail.html')
+
 
 @app.route('/edit')
 def edit():
     return render_template('edit.html')
 
+
 @app.route('/formaddmenu')
 def form_add_menu():
     return render_template('form_add_menu.html')
+
 
 @app.route('/homepageadmin')
 def homepage_admin():
     return render_template('homepage_admin.html')
 
+
 @app.route('/kategori')
 def kategori():
     return render_template('kategori.html')
+
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+
 @app.route('/popular')
 def popular():
     return render_template('popular.html')
+
 
 @app.route('/register')
 def register():
     return render_template('register.html')
 
+
 @app.route('/review')
 def review():
     return render_template('review.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
