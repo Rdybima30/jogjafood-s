441d7f66086c4ea6a98a31effc53bc0e7a04d680
Nada Anis Nurjihan <nurjinurjihan26@gmail.com>
2023-12-15 09:19:20 +0700

    commit regis

--- app.py
+++ app.py
@@ -14,39 +14,95 @@ app.config['UPLOAD_FOLDER'] = './static/profile_pics'
 
 SECRET_KEY = "SPARTA"
 
-MONGODB_CONNECTION_STRING = "mongodb+srv://rendybima:rendybima(301002)@cluster0.6zfd4hh.mongodb.net/?retryWrites=true&w=majority"
+MONGODB_CONNECTION_STRING = "mongodb+srv://nadaanis526:nada2626anis@cluster0.zp4go5w.mongodb.net/"
 client = MongoClient(MONGODB_CONNECTION_STRING)
 db = client.jogjafood
 
-TOKEN_KEY = "mytoken"
-
+SECRET_KEY = 'secret1141'
+TOKEN_KEY = 'mytoken'
 
 @app.route('/')
-def homepage():
-    return render_template('homepage.html')
+def main():
+    return render_template('index.html')
 
-@app.route('/signup')
-def signup():
+@app.route('/home', methods=['GET'])
+def home():
+    token_receive = request.cookies.get(TOKEN_KEY)
+    try:
+        payload = jwt.decode(
+            token_receive,
+            SECRET_KEY,
+            algorithms=['HS256']
+        )
+        user_info = db.users.find_one({"email": payload["id"]})
+        is_admin = user_info.get("category") == "admin"
+        logged_in = True
+        print(user_info)
+        return render_template('homepage.html', user_info=user_info, logged_in=logged_in, is_admin=is_admin)
+    except jwt.ExpiredSignatureError:
+        msg = 'Your token has expired'
+    except jwt.exceptions.DecodeError:
+        msg = 'There was a problem logging you in'
+    return render_template('homepage.html', msg=msg)
+
+@app.route('/register')
+def register():
     return render_template('register.html')
 
+@app.route('/signup',methods=['POST'])
+def api_register():
+    name=request.form.get('name')
+    email=request.form.get('email')
+    password=request.form.get('password')
 
-@app.route('/sign_up/save', methods=['POST'])
-def sign_up():
-    name = request.form.get('name')
-    email = request.form.get('email')
-    password = request.form.get('password')
-    password_hash = hashlib.sha256(password. encode('utf-8')).hexdigest()
-    doc = {
-        "name": name,
-        "email": email,
+    pw_hash=hashlib.sha256(password.encode('utf-8')).hexdigest() #mengenskripsi pw
+
+    db.user.insert_one({
+        'name':name,
+        'email':email,
         "category": 'visitor',
-        "password": password_hash
-    }
-    db.users.insert_one(doc)
-    return jsonify({'result': 'success'})
+        'password':pw_hash
+    })
+    return  jsonify({'result':'success'})
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
+@app.route("/signin", methods=["POST"])
+def signin():
+    email = request.form["email"]
+    password = request.form["password"]
+    print(email)
+    pw_hash = hashlib.sha256(password.encode("utf-8")).hexdigest()
+    print(pw_hash)
+    result = db.users.find_one(
+        {
+            "email": email,
+            "password": pw_hash,
+        }
+    )
+    if result:
+        payload = {
+            "id": email,
+            "exp": datetime.utcnow() + timedelta(seconds=60 * 60 * 24),
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
 
+        return jsonify(
+            {
+                "result": "success",
+                "token": token,
+            }
+        )
+    else:
+        return jsonify(
+            {
+                "result": "fail",
+                "msg": "We could not find a user with that id/password combination",
+            }
+        )
+    
 
 @app.route('/addmenu')
 def add_menu():
@@ -77,21 +133,9 @@ def homepage_admin():
 def kategori():
     return render_template('kategori.html')
 
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-
 @app.route('/popular')
 def popular():
     return render_template('popular.html')
-
-
-@app.route('/register')
-def register():
-    return render_template('register.html')
-
 
 @app.route('/review')
 def review():
