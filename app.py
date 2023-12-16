d32b050966fe0e21dd579055659d163057a7fb24
Nada Anis Nurjihan <nurjinurjihan26@gmail.com>
2023-12-16 09:34:33 +0700

    bismillah

--- app.py
+++ app.py
@@ -130,6 +130,18 @@ def popular():
 @app.route("/review")
 def review():
     return render_template("review.html")
+
+@app.route("/homepageadmin")
+def homepageadmin():
+    return render_template("homepage_admin.html")
+
+@app.route("/addmenu")
+def addmenu():
+    return render_template("addmenu.html")
+
+@app.route("/formaddmenu")
+def formaddmenu():
+    return render_template("form_add_menu.html")
     
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
